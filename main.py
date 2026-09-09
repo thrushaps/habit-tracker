3d8b8d63d989452320bc272248240361f00528ca
thrushaps <thrushaps@gmail.com>
2026-09-10 00:01:40 +0530

    next way

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+import streamlit as st
 import json
+import os
 from datetime import datetime
 from collections import Counter
+
+# ================= LOGIN SYSTEM =================
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+if not st.session_state.logged_in:
+    st.title("🔐 Login Page")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if username == "thrusha" and password == "thrusha":
+            st.session_state.logged_in = True
+            st.success("Login successful")
+            st.rerun()
+        else:
+            st.error("Invalid username or password")
+
+    st.stop()
 
 FILE = "data.json"
 
